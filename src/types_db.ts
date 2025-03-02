6c4b79c9dde00e407cfe390b5f9f5002aae96757
82orez <82orez@naver.com>
2025-03-02 11:07:35 +0900

    supabase settings

---
+++ src/types_db.ts
@@ -0,0 +1,469 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      _prisma_migrations: {
+        Row: {
+          applied_steps_count: number
+          checksum: string
+          finished_at: string | null
+          id: string
+          logs: string | null
+          migration_name: string
+          rolled_back_at: string | null
+          started_at: string
+        }
+        Insert: {
+          applied_steps_count?: number
+          checksum: string
+          finished_at?: string | null
+          id: string
+          logs?: string | null
+          migration_name: string
+          rolled_back_at?: string | null
+          started_at?: string
+        }
+        Update: {
+          applied_steps_count?: number
+          checksum?: string
+          finished_at?: string | null
+          id?: string
+          logs?: string | null
+          migration_name?: string
+          rolled_back_at?: string | null
+          started_at?: string
+        }
+        Relationships: []
+      }
+      Account: {
+        Row: {
+          access_token: string | null
+          createdAt: string
+          expires_at: number | null
+          id_token: string | null
+          provider: string
+          providerAccountId: string
+          refresh_token: string | null
+          refresh_token_expires_in: number | null
+          scope: string | null
+          session_state: string | null
+          token_type: string | null
+          type: string
+          updatedAt: string
+          userId: string
+        }
+        Insert: {
+          access_token?: string | null
+          createdAt?: string
+          expires_at?: number | null
+          id_token?: string | null
+          provider: string
+          providerAccountId: string
+          refresh_token?: string | null
+          refresh_token_expires_in?: number | null
+          scope?: string | null
+          session_state?: string | null
+          token_type?: string | null
+          type: string
+          updatedAt: string
+          userId: string
+        }
+        Update: {
+          access_token?: string | null
+          createdAt?: string
+          expires_at?: number | null
+          id_token?: string | null
+          provider?: string
+          providerAccountId?: string
+          refresh_token?: string | null
+          refresh_token_expires_in?: number | null
+          scope?: string | null
+          session_state?: string | null
+          token_type?: string | null
+          type?: string
+          updatedAt?: string
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Account_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      CompletedSentence: {
+        Row: {
+          completedAt: string
+          id: string
+          sentenceId: number
+          userId: string
+        }
+        Insert: {
+          completedAt?: string
+          id: string
+          sentenceId: number
+          userId: string
+        }
+        Update: {
+          completedAt?: string
+          id?: string
+          sentenceId?: number
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "CompletedSentence_sentenceId_fkey"
+            columns: ["sentenceId"]
+            isOneToOne: false
+            referencedRelation: "Sentence"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "CompletedSentence_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      EmailVerificationToken: {
+        Row: {
+          createdAt: string
+          email: string
+          expires: string
+          id: string
+          token: string
+        }
+        Insert: {
+          createdAt?: string
+          email: string
+          expires: string
+          id: string
+          token: string
+        }
+        Update: {
+          createdAt?: string
+          email?: string
+          expires?: string
+          id?: string
+          token?: string
+        }
+        Relationships: []
+      }
+      PasswordResetToken: {
+        Row: {
+          createdAt: string
+          email: string
+          expires: string
+          id: string
+          token: string
+        }
+        Insert: {
+          createdAt?: string
+          email: string
+          expires: string
+          id: string
+          token: string
+        }
+        Update: {
+          createdAt?: string
+          email?: string
+          expires?: string
+          id?: string
+          token?: string
+        }
+        Relationships: []
+      }
+      QuizAttempt: {
+        Row: {
+          attemptedAt: string
+          correct: boolean
+          id: string
+          sentenceId: number
+          userId: string
+        }
+        Insert: {
+          attemptedAt?: string
+          correct: boolean
+          id: string
+          sentenceId: number
+          userId: string
+        }
+        Update: {
+          attemptedAt?: string
+          correct?: boolean
+          id?: string
+          sentenceId?: number
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "QuizAttempt_sentenceId_fkey"
+            columns: ["sentenceId"]
+            isOneToOne: false
+            referencedRelation: "Sentence"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "QuizAttempt_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      Sentence: {
+        Row: {
+          en: string
+          id: number
+          ko: string
+          no: number
+        }
+        Insert: {
+          en: string
+          id?: number
+          ko: string
+          no: number
+        }
+        Update: {
+          en?: string
+          id?: number
+          ko?: string
+          no?: number
+        }
+        Relationships: []
+      }
+      Session: {
+        Row: {
+          createdAt: string
+          expires: string
+          sessionToken: string
+          updatedAt: string
+          userId: string
+        }
+        Insert: {
+          createdAt?: string
+          expires: string
+          sessionToken: string
+          updatedAt: string
+          userId: string
+        }
+        Update: {
+          createdAt?: string
+          expires?: string
+          sessionToken?: string
+          updatedAt?: string
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Session_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      Test: {
+        Row: {
+          age: number | null
+          created_at: string
+          id: number
+          name: string
+          updated_at: string | null
+        }
+        Insert: {
+          age?: number | null
+          created_at?: string
+          id?: number
+          name: string
+          updated_at?: string | null
+        }
+        Update: {
+          age?: number | null
+          created_at?: string
+          id?: number
+          name?: string
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
+      User: {
+        Row: {
+          createdAt: string
+          credentials: boolean
+          email: string
+          emailVerified: string | null
+          id: string
+          image: string | null
+          name: string | null
+          password: string | null
+          updatedAt: string
+        }
+        Insert: {
+          createdAt?: string
+          credentials?: boolean
+          email: string
+          emailVerified?: string | null
+          id: string
+          image?: string | null
+          name?: string | null
+          password?: string | null
+          updatedAt: string
+        }
+        Update: {
+          createdAt?: string
+          credentials?: boolean
+          email?: string
+          emailVerified?: string | null
+          id?: string
+          image?: string | null
+          name?: string | null
+          password?: string | null
+          updatedAt?: string
+        }
+        Relationships: []
+      }
+      VerificationToken: {
+        Row: {
+          expires: string
+          identifier: string
+          token: string
+        }
+        Insert: {
+          expires: string
+          identifier: string
+          token: string
+        }
+        Update: {
+          expires?: string
+          identifier?: string
+          token?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
