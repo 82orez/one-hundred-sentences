884eb621fe7dc9696c5aeaf9128c64138af911ef
82orez <82orez@naver.com>
2025-03-12 05:07:42 +0900

    recorder Modal-1 bg-color

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,7 +11,7 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="bg-opacity-50 fixed inset-0 flex items-center justify-center bg-black">
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-400/10 backdrop-blur-xs">
       <div className="relative w-full max-w-lg rounded-lg bg-white p-6 shadow-lg">
         <button className="absolute top-4 right-4 text-gray-600 hover:text-gray-900" onClick={onClose}>
           <RiCloseLargeFill size={24} />
