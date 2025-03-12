ce5d0615674d40ef28168ccce0f0ea0a6bf63c8e
82orez <82orez@naver.com>
2025-03-12 16:39:01 +0900

    recorder Modal-Ko-3

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,7 +11,7 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-400/10 backdrop-blur-xs">
+    <div className="fixed inset-0 mt-10 flex items-center justify-center bg-gray-400/10 backdrop-blur-xs">
       <div className="relative flex w-full max-w-[360px] justify-center rounded-lg bg-white p-6 shadow-lg">
         {/*<button className="absolute top-4 right-4 text-gray-600 hover:text-gray-900" onClick={onClose}>*/}
         {/*  <RiCloseLargeFill size={24} />*/}
