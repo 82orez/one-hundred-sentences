08e629465eec32e68a6da07e9e52f31a93eae037
82orez <82orez@naver.com>
2025-03-30 15:28:22 +0900

    minor

--- src/app/api/review/route.ts
+++ src/app/api/review/route.ts
@@ -16,21 +16,21 @@ export async function GET(req: Request) {
       select: { sentenceNo: true },
     });
 
-    // ✅ 학습일별 완료된 문장 개수를 계산
+    // ✅ 학습일별 완료된 문장 개수를 계산 - Object(객체)
     const dayCounts: Record<number, number> = {};
     completedSentences.forEach(({ sentenceNo }) => {
       const day = Math.ceil(sentenceNo / 5);
       dayCounts[day] = (dayCounts[day] || 0) + 1;
     });
 
-    console.log("dayCounts: ", dayCounts);
+    console.log("dayCountsObject: ", dayCounts);
 
-    // ✅ 5문장을 모두 완료한 학습일만 필터링
+    // ✅ 5문장을 모두 완료한 학습일만 필터링 - Array(배열)
     const completedDays = Object.keys(dayCounts)
       .map(Number)
       .filter((day) => dayCounts[day] === 5); // ✅ 학습일에 속한 5문장이 전부 완료된 경우만 반환
 
-    console.log("completedDays: ", completedDays);
+    console.log("completedDaysArray: ", completedDays);
     return NextResponse.json({ completedDays });
   } catch (error) {
     console.error("Failed to fetch review data:", error);
