a8836ec5dcc0d50507094fc4e29cbd289826c7a2
82orez <82orez@naver.com>
2025-05-13 09:50:20 +0900

    home page-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,16 @@ export default async function LandingPage() {
       <div className="w-full px-4 md:px-8">
         <Card className="mx-auto flex w-full max-w-4xl flex-col items-center rounded-2xl bg-white p-6 text-gray-800 shadow-xl md:flex-row">
           <Image src="/images/english-learning.png" width={300} height={300} alt="영어 학습" className="mb-4 rounded-lg shadow-md md:mr-6 md:mb-0" />
-          <Button className="mb-4 flex cursor-pointer items-center rounded-full bg-indigo-600 px-6 py-3 text-lg text-white hover:bg-indigo-700 md:hidden">
-            개인 회원 <FaArrowRight className="ml-2" />
-          </Button>
 
-          <Button className="mb-4 flex cursor-pointer items-center rounded-full bg-indigo-600 px-6 py-3 text-lg text-white hover:bg-indigo-700 md:hidden">
+          <Button className="mb-4 flex min-w-[220px] cursor-pointer items-center rounded-full bg-indigo-600 px-6 py-3 text-lg text-white hover:bg-indigo-700 md:hidden">
             문화센터 등록 회원 <FaArrowRight className="ml-2" />
           </Button>
+
+          <Button className="mb-4 flex min-w-[220px] cursor-pointer items-center justify-center rounded-full bg-indigo-600 px-6 py-3 text-lg text-white hover:bg-indigo-700 md:hidden">
+            <span>개인 회원</span>
+            <FaArrowRight className="ml-2" />
+          </Button>
+
           <CardContent className="text-center md:text-left">
             <h2 className="mb-1 text-xl font-semibold md:mb-3 md:text-2xl">영어 100문장으로 영어의 기초를 완성하세요.</h2>
             <p className="mb-4 text-gray-600">반복 학습과 체계적인 암기법을 통해 누구나 쉽게 영어 실력을 높일 수 있습니다.</p>
