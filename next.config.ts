ea9d3fd7531b591c6a288a7d504bfaa2a0e5f9db
82orez <82orez@naver.com>
2025-05-26 15:53:39 +0900

    user dashboard one team-2

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: "",
         search: "",
       },
+      {
+        protocol: "https",
+        hostname: "krgsfzhqitjtaasgupsv.supabase.co",
+        port: "",
+        search: "",
+      },
     ],
   },
 };
