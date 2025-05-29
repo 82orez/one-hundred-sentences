9e946c7c6648700c78453d3d8d75a1ed15ef6ff9
82orez <82orez@naver.com>
2025-05-30 08:34:56 +0900

    home page free course added-5

--- src/app/users/my-courses/page.tsx
+++ src/app/users/my-courses/page.tsx
@@ -188,8 +188,8 @@ export default function MyCoursesPage() {
                         수강중
                       </span>
                     </div>
-                    <p className="mb-1 text-gray-600">강사: {enrollment.course.teacher.user.realName}</p>
-                    <p className="mb-4 text-sm text-gray-600">이메일: {enrollment.course.teacher.user.email}</p>
+                    <p className="mb-1 text-gray-600">강사: {enrollment.course.teacher?.user.realName || "-"}</p>
+                    <p className="mb-4 text-sm text-gray-600">이메일: {enrollment.course.teacher?.user.email || "-"}</p>
                     {/*<p className="mb-4 text-sm text-gray-600">Zoom: {enrollment.course.teacher.user.zoomInviteUrl}</p>*/}
 
                     {/*<p className="mb-4 text-gray-600">전화번호: {enrollment.course.teacher.user.phone}</p>*/}
