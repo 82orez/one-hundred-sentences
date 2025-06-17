e5972a98856a7a4809bf0de8f86463c7c532f456
82orez <82orez@naver.com>
2025-06-17 18:35:07 +0900

    purchase page updated-3

--- src/lib/pricePlans.ts
+++ src/lib/pricePlans.ts
@@ -5,7 +5,7 @@ export const pricePlans = [
     price: 0,
     description: [
       {
-        title: "온라인 무료 특강 제공",
+        title: "실시간 온라인 무료 특강 진행",
         details: ["매주 일요일 오후 8시", "15분 수업", "zoom 사전 설치 필요"],
       },
       // "기본 학습 어플 제공",
