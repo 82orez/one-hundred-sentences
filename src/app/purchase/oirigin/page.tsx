a6d59762c11c99ee278f9ba0c5ed32c73c271aff
82orez <82orez@naver.com>
2025-06-18 05:43:56 +0900

    purchase page updated-4

--- src/app/purchase/oirigin/page.tsx
+++ src/app/purchase/oirigin/page.tsx
@@ -63,7 +63,7 @@ const PurchasePage = () => {
                   </div>
                 )}
                 <h3 className={`text-xl font-bold ${selectedPlan === plan.id ? "text-blue-700" : "text-blue-600"}`}>{plan.title}</h3>
-                <p className="mt-4 text-sm text-gray-600">{plan.description}</p>
+                {/*<p className="mt-4 text-sm text-gray-600">{plan.description}</p>*/}
               </div>
             ))}
           </div>
