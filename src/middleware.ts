d6c59b6b934bf724058cdf743e62363358001a7e
82orez <82orez@naver.com>
2025-07-24 05:01:40 +0900

    middleware.ts /purchase changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,5 @@ export { default } from "next-auth/middleware";
 // 로그인을 하지 않은 상태에서 /server-side-page 경로로 접근하면 authOptions 에서 설정한 signIn page 로 라우트 됨.
 // /:path*" 의 의미는 해당 경로의 모든 하위 경로를 포함한다는 의미.
 export const config = {
-  matcher: ["/learn/:path*", "/users/edit", "/users/profile", "/users/my-courses", "/dashboard/:path*", "/purchase/:path*"],
+  matcher: ["/learn/:path*", "/users/edit", "/users/profile", "/users/my-courses", "/dashboard/:path*", "/purchase/courses", "/purchase/free"],
 };
